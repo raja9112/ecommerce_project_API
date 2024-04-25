7a01cc06c81942474935d954ea0059cdd1f9e48f
raja9112 <rrn.rajarajan@gmail.com>
2024-04-26 01:55:53 +0530

    updated urls.py app level

--- ecommerce_project_api/ecommerce_API/urls.py
+++ ecommerce_project_api/ecommerce_API/urls.py
@@ -3,7 +3,7 @@ from . import views
 urlpatterns =[
     path('categories/', views.categoriesView.as_view()),
     path('Menu-Items/', views.MenuItemView.as_view()),
-    path('SingleMenuItem/<int:pk>/', views.SingleMenuItemView.as_view()),
+    path('MenuItem/<int:pk>/', views.SingleMenuItemView.as_view()),
     path('Cart/', views.CartView.as_view()),
     
 ]
