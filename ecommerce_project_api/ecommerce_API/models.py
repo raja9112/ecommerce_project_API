adad33d95efb0809ce2c96e857d91c3d4d19d55c
raja9112 <rrn.rajarajan@gmail.com>
2024-04-25 21:37:38 +0530

    Created tables, registered in admin sites, tested authtokens in Insomnia

--- ecommerce_project_api/ecommerce_API/models.py
+++ ecommerce_project_api/ecommerce_API/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    slug = models.SlugField()
+    title = models.CharField(max_length=255, db_index=True)
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=255, db_index = True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
+    featured = models.BooleanField(default=False, db_index=True)
+    category = models.ForeignKey(Category, on_delete= models.PROTECT)
+    
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
+    menu_item = models.ForeignKey(MenuItem, on_delete= models.CASCADE)
+    stock = models.SmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
+    
+    class Meta:  # One customer should not have more than 1 same product in cart instead of they can change the stock quantity.
+        unique_together = ('user', 'menu_item')
+        
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    delivery_crew = models.ForeignKey(User, on_delete=models.CASCADE, related_name= "delivery_crew", null= True)
+    total = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
+    status = models.BooleanField(default=False, db_index=True)
+    date = models.DateField(db_index=True)
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT, related_name='order')
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.PROTECT)
+    quantity = models.SmallIntegerField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    
+    class Meta:
+        unique_together = ('order', 'menu_item')
+        
--- ecommerce_project_api/ecommerce_project_api/settings.py
+++ ecommerce_project_api/ecommerce_project_api/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'djoser',
     'rest_framework.authtoken',
-    'django_filters',
+    # 'rest_framework_filters',
 ]
 
 MIDDLEWARE = [
@@ -123,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES":[
